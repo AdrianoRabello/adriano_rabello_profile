ed9b0c6643b4e4a33ddebadcd7fba94c3eee6c03
adrianorabello <adrianor.rabello@hotmail.com>
2022-03-19 16:44:08 -0300

    media query created

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,8 +6,9 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
 
+
 const navItems = document.querySelectorAll(".nav-item");
-console.log("navitens ", navItems)
+console.log("menuNav ", menuNav)
 
 // set initial of menu 
 
